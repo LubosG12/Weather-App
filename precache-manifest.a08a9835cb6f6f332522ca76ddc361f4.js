752c5863b61db49e395ab482f1b91139380e5ebc
Lubomir Furinda <l.furinda@gmail.com>
2019-12-19 16:38:50 +0100

    Updates

---
+++ precache-manifest.a08a9835cb6f6f332522ca76ddc361f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beb1815dea8af72a3ec1feb82a97c9dc",
+    "url": "/Weather-App/index.html"
+  },
+  {
+    "revision": "b09ad80750d51e148d77",
+    "url": "/Weather-App/static/css/main.04f78a90.chunk.css"
+  },
+  {
+    "revision": "f7ece7a5e3318c4c527d",
+    "url": "/Weather-App/static/js/2.89c384df.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Weather-App/static/js/2.89c384df.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b09ad80750d51e148d77",
+    "url": "/Weather-App/static/js/main.a94774e0.chunk.js"
+  },
+  {
+    "revision": "7e6827bb9bbf03162d47",
+    "url": "/Weather-App/static/js/runtime-main.bd3a7e63.js"
+  }
+]);
